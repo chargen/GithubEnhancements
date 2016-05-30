1fe0bcdd68c13b11e71c6f1e3c854cd9a187162f
Martin Evans <martindevans@gmail.com>
2016-05-30 17:47:22 +0100

    Made issue page modifier only run on pages with "issues/" in the url
    Signed-off-by: Martin Evans <martindevans@gmail.com>

--- Github_Issue_Page.js
+++ Github_Issue_Page.js
@@ -8,13 +8,19 @@
 // @grant        none
 // @downloadURL  https://raw.githubusercontent.com/martindevans/GithubEnhancements/master/Github_Issue_Page.js
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
-// @require      https://gist.githubusercontent.com/martindevans/d0470b25664150145603ae6a71074445/raw/9c97aa67ff9c5d56be34a55ad6c18a314e5eb548/waitForKeyElements.js
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
 // ==/UserScript==
 
 (function() {
     'use strict';
 
+    var urlCheck = function() {
+         return window.location.href.includes("issues/");
+    };
+
     waitForKeyElements("#new_comment_field", function() {
+        if (!urlCheck()) return;
+
         var txtarea = document.getElementById("new_comment_field");
         if (txtarea) {
 
@@ -39,6 +45,8 @@
     });
 
     waitForKeyElements(".container.new-discussion-timeline.experiment-repo-nav", function() {
+        if (!urlCheck()) return;
+
         //Make entire issue form larger
         var issueFormContainer = document.getElementsByClassName("container new-discussion-timeline experiment-repo-nav");
         if (issueFormContainer.length === 1)
@@ -48,6 +56,8 @@
     });
 
     waitForKeyElements(".discussion-timeline.js-quote-selection-container", function() {
+        if (!urlCheck()) return;
+
         //Make comment section container fill extra width
         var commentContainer = document.getElementsByClassName("discussion-timeline js-quote-selection-container");
         if (commentContainer.length === 1)
@@ -57,6 +67,8 @@
     });
 
     waitForKeyElements(".timeline-comment-wrapper.timeline-new-comment.js-comment-container", function() {
+        if (!urlCheck()) return;
+
         //Make submission form fill extra width
         var submitForm = document.getElementsByClassName("timeline-comment-wrapper timeline-new-comment js-comment-container");
         if (submitForm.length === 1)
